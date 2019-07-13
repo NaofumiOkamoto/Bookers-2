e031eb033d54e3987448608f71f1c1ded65a6a7f
NaofumiOkamoto <n.okamooo@gmail.com>
2019-07-13 12:51:47 +0900

    [Update] header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,20 @@
 class ApplicationController < ActionController::Base
 
-  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   # deviseを利用する機能が実行される前に実行する
 
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
 
 
   protected  # 他のコントローラからも参照できるストロングパラメーター
-#-----------教材のやり方------------------
-#  def configure_permitted_parameters
-#    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-#    # sign_upでnameのデータ操作を許可
-#  end
-
-#-----------nameとpassでログインするやり方-------------
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    # sign_upでnameのデータ操作を許可
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'users#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :index, :update]
+  resources :users, only: [:show, :edit, :index, :update, :top]
 end
